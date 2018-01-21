0899eb1c1406faadf3ba7d2d3592db6bdbd733a3
coffe <coffeacc10@gmail.com>
2018-01-21 23:06:50 +0200

    First commit

---
+++ SeleniumTest1.java
@@ -0,0 +1,29 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SeleniumTest1 {
+
+
+    private WebDriver driver;
+
+
+    @Before
+    public void setUp(){
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+    }
+
+    @After
+    public void tearDown(){
+        driver.close();
+    }
+
+    @Test
+    public void test1 (){
+        driver.get("Https:facedrive.com");
+    }
+
+}
